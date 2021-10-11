2a691042a97ff18495ac55ef5f1b60f08c6e1ae9
Michael Cullen <michaelcullen2011@hotmail.co.uk>
2021-10-11 12:04:43 +0100

    calculates the average perc increase/decrease for specified tickers based on days of the week

--- scripts/analysis_scripts/best_day.py
+++ scripts/analysis_scripts/best_day.py
@@ -1,6 +1,7 @@
 import os
 import csv
-from datetime import datetime
+from datetime import datetime as dt
+import datetime
 
 import numpy as np
 import pandas as pd
@@ -11,12 +12,65 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 _root = os.getcwd()
-_portfolio = os.path.join(_root, 'datasets\\portfolio\\')
-_plots = os.path.join(_portfolio, 'plots')
 
-def run():
-    pass
+yf.pdr_override()
 
-if __name__=="__main__":
-    run()
+
+def get_data(ticker, timescale):
+    today = dt.today()
+    start_date = today - datetime.timedelta(days=365 * timescale)
+    if "USD" in ticker:
+        return pdr.DataReader(
+            ticker,
+            data_source="yahoo",
+            start=start_date.strftime("%Y-%m-%d"),
+        )["Adj Close"]
+    else:
+        return pdr.get_data_yahoo(
+            ticker,
+            start=start_date.strftime("%Y-%m-%d"),
+        )["Adj Close"]
+
+
+def calc_perc_change(data):
+    perc_change = data.pct_change() * 100
+    return perc_change
+
+
+def get_days_col(data):
+    weekdays = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    data = data.reset_index()
+    data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+    data["Weekday"] = data["Date"].dt.day_name()
+    return data
+
+
+def aggregate(data):
+    return data[["Adj Close", "Weekday"]].groupby(["Weekday"]).mean()
+
+
+def run(tickers, timescale):
+    data = {}
+    perc_change = {}
+    weekday_perc_change = {}
+    aggregated_data = {}
+    for ticker in tickers:
+        data[ticker] = get_data(ticker, timescale)
+        perc_change[ticker] = calc_perc_change(data[ticker])
+        weekday_perc_change[ticker] = get_days_col(perc_change[ticker])
+        aggregated_data[ticker] = aggregate(weekday_perc_change[ticker])
+    print(aggregated_data)
+
+
+if __name__ == "__main__":
+    tickers = ["GME", "BTC-USD", "ETH-USD"]
+    run(tickers=tickers, timescale=0.25)
     plt.show()
